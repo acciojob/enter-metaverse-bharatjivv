41cb478bab974dac10159ffdc0ac581521f2fdf6
Bharat <hibharatj@gmail.com>
2025-06-17 18:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required.https://www.svgrepo.com/show/345221/three-dots.svg
+
+
+let btn = document.getElementById('enterBtn');
+let status = document.getElementById('status');
+
+
+btn.addEventListener('click', () => {
+	status.innerHTML = `<h1> Entered Metaverse </h1>`
+})
